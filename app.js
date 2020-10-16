7b8ceee15b579d982831ad829eec6164bd87dd47
Rohitverma05 <63829615+Rohitverma05@users.noreply.github.com>
2020-10-17 02:24:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post("/job",(req,res)=>{
 
 });
 
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log("server started at port 3000");
+app.listen(process.env.PORT || 80, ()=>{
+  console.log("server started at port 80");
 });
